c8666ff7ef2e65312e8a8f4b884b26c5cc1a2a78
Danilo Faraum <rabelofaraumdanilo0123@gmail.com>
2023-05-23 21:46:56 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { isMobile } from 'react-device-detect';
 import './App.css';
+import retry from './assets/retry.svg'
 
 function App() {
+  const [quoteStyle, setQuoteStyle] = useState({fontSize: 48})
+  const [quote, setQuote] = useState("Have Fun!")
+
+  const random = async () =>{
+    const url = "https://api.kanye.rest"
+    const response = await fetch(url)
+    const json = await response.json()
+    setQuote(await json.quote)
+    if (isMobile){
+      setQuoteStyle({fontSize: 20})
+    }
+    else{
+      if (JSON.stringify(json).length >90){
+        setQuoteStyle({fontSize: 30})
+      }
+      else{
+        setQuoteStyle({fontSize: 48})
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className='happy' src="https://freepngimg.com/download/kanye_west/1-2-kanye-west-png-hd.png" alt="" />
+      <main>
+        <h1>RANDOM KANYE <br/>QUOTE</h1>
+        <div className="quote-space">
+          <div style={quoteStyle} className="quote">{quote}</div>
+          <p className="kanye-west">-Kanye West</p>
+        </div>
+        <div onClick={random} className="button">
+          RANDOM <img className='retry' src={retry} alt="" />
+        </div>
+      </main>
+      <img className='angry' src="https://www.seekpng.com/png/full/12-126395_kanye-head-png-kanye-west-transparent-background.png" alt="" />
     </div>
   );
 }
